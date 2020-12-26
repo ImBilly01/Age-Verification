31c032c6f8dc8c2e2d77f92942465c5b5bea3230
armado <56002652+armado57@users.noreply.github.com>
2020-12-26 07:46:41 +0100

    Add files via upload

---
+++ Python Age Verification/ageverification.py
@@ -0,0 +1,22 @@
+# === Start of code ===
+
+age = int(input("How old are you? "))
+print("")
+
+if age < 18:
+  print("You can't drink.")
+  print("")
+
+elif age == 18 or age == 19:
+  print("You are new to this!")
+  print("")
+
+elif age == 20:
+  print("You are still kind of young.")
+  print("")
+
+else:
+  print("Enjoy your drink!")
+  print("")
+
+# === End of code ===
